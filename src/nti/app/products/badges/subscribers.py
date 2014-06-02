9b746eebbd3587f7dc759be50bbc08e9fbbc4897
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-02 02:54:37 +0000

    don't adapt
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@40221 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/subscribers.py
+++ src/nti/app/products/badges/subscribers.py
@@ -11,7 +11,6 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 
-from nti.badges import interfaces as badges_interfaces
 from nti.badges.tahrir import interfaces as tahrir_interfaces
 
 from nti.dataserver import interfaces as nti_interfaces
@@ -22,9 +21,8 @@ from . import get_user_badge_managers
 
 @component.adapter(nti_interfaces.IUser, IObjectRemovedEvent)
 def _user_deleted(user, event):
-    person = badges_interfaces.INTIPerson(user)
     for manager in get_user_badge_managers(user):
-        manager.delete_person(person)
+        manager.delete_person(user)
 
 @component.adapter(IAfterDatabaseOpenedEvent)
 def _after_database_opened_listener(event):

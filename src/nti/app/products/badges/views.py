2224bfd29b676d939642bb7c18f2ba1b93677011
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-05 02:43:39 +0000

    api to get an open assertion
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@40534 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/views.py
+++ src/nti/app/products/badges/views.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-pyramid views.
-
 .. $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
@@ -12,6 +10,7 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 from zope import interface
+from zope.catalog.interfaces import ICatalog
 from zope.location.interfaces import IContained
 from zope.container import contained as zcontained
 from zope.traversing.interfaces import IPathAdapter
@@ -26,6 +25,7 @@ from nti.badges.openbadges import interfaces as badge_interfaces
 
 from nti.dataserver.users import User
 from nti.dataserver import authorization as nauth
+from nti.dataserver.users import index as user_index
 from nti.dataserver import interfaces as nti_interfaces
 
 from nti.utils import maps
@@ -73,7 +73,8 @@ class OpenBadgeView(object):
 			badge = splits[-1]
 		else:
 			params = maps.CaseInsensitiveDict(request.params)
-			badge = params.get('badge', params.get('badge_name', params.get('badgeName')))
+			badge = params.get('badge') or params.get('badge_name') or \
+					params.get('badgeName')
 
 		if not badge:
 			raise hexc.HTTPUnprocessableEntity('Badge not specified')
@@ -84,3 +85,37 @@ class OpenBadgeView(object):
 				return badge_interfaces.IBadgeClass(result)
 
 		raise hexc.HTTPNotFound('Badge not found')
+
+OPEN_ASSERTIONS_VIEW = 'OpenAssertions'
+
+@view_config(route_name='objects.generic.traversal',
+			 name=OPEN_ASSERTIONS_VIEW,
+			 renderer='rest',
+			 request_method='GET',
+			 context=nti_interfaces.IDataserverFolder,
+			 permission=nauth.ACT_READ)
+class OpenAssertionsView(object):
+
+	def __init__(self, request):
+		self.request = request
+
+	def __call__(self):
+		request = self.request
+		params = maps.CaseInsensitiveDict(request.params)
+		username = params.get('user') or params.get('username') or params.get('email')
+		badge = params.get('badge') or params.get('badge_name') or params.get('badgeName')
+
+		user = User.get_user(username)
+		if user is None:
+			ent_catalog = component.getUtility(ICatalog, name=user_index.CATALOG_NAME)
+			results = list(ent_catalog.searchResults(email=(username, username)))
+			user = results[0] if results else None
+		if user is None:
+			raise hexc.HTTPNotFound('User not found')
+
+		for manager in get_user_badge_managers(user):
+			result = manager.get_assertion(user, badge)
+			if result is not None:
+				return badge_interfaces.IBadgeAssertion(result)
+
+		raise hexc.HTTPNotFound('Assertion not found')

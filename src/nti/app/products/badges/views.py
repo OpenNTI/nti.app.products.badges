b3a60124f61d93ced7b3b0c5704f421ce15e8c5b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-16 00:38:35 +0000

    use a path adapter to the /OpenAssertions path
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@41392 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/views.py
+++ src/nti/app/products/badges/views.py
@@ -12,7 +12,6 @@ import urllib
 
 from zope import component
 from zope import interface
-from zope.catalog.interfaces import ICatalog
 from zope.location.interfaces import IContained
 from zope.container import contained as zcontained
 from zope.traversing.interfaces import IPathAdapter
@@ -21,19 +20,19 @@ from pyramid.view import view_config
 from pyramid.interfaces import IRequest
 from pyramid import httpexceptions as hexc
 
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
 from nti.appserver.interfaces import IUserService
 
 from nti.badges import interfaces as badge_interfaces
 from nti.badges.openbadges import interfaces as open_interfaces
 
-from nti.dataserver.users import User
 from nti.dataserver import authorization as nauth
-from nti.dataserver.users import index as user_index
 from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.authorization_acl import ace_allowing
+from nti.dataserver.authorization_acl import acl_from_aces
 
 from nti.externalization.interfaces import LocatedExternalDict
-
-from nti.utils import maps
 
 from . import interfaces
 from . import get_all_badges
@@ -82,48 +81,39 @@ class OpenBadgeView(object):
 
 		raise hexc.HTTPNotFound('Badge not found')
 
+@interface.implementer(IPathAdapter)
+@component.adapter(nti_interfaces.IDataserverFolder, IRequest)
+class OpenAssertionsPathAdapter(zcontained.Contained):
+
+	def __init__(self, dataserver, request):
+		self.__parent__ = dataserver
+		self.__name__ = OPEN_ASSERTIONS_VIEW
+
+	def __getitem__(self, assertion_id):
+		if not assertion_id:
+			raise KeyError(assertion_id)
+
+		assertion_id = urllib.unquote(assertion_id)
+		manager = component.getUtility(badge_interfaces.IBadgeManager)
+		result = manager.get_assertion_by_id(assertion_id)
+		if result is not None:
+			result = open_interfaces.IBadgeAssertion(result)
+			result. __acl__ = acl_from_aces(
+								ace_allowing(nti_interfaces.AUTHENTICATED_GROUP_NAME,
+											 nauth.ACT_READ))
+			return result
+
+		raise KeyError(assertion_id)
+
 @view_config(route_name='objects.generic.traversal',
-			 name=OPEN_ASSERTIONS_VIEW,
 			 renderer='rest',
 			 request_method='GET',
-			 context=nti_interfaces.IDataserverFolder,
+			 context=OpenAssertionsPathAdapter,
 			 permission=nauth.ACT_READ)
-class OpenAssertionsView(object):
-
-	def __init__(self, request):
-		self.request = request
+class OpenAssertionsView(AbstractAuthenticatedView):
 
 	def __call__(self):
-		result = None
-		request = self.request
-		params = maps.CaseInsensitiveDict(request.params)
-		username = params.get('user') or params.get('username') or params.get('email')
-		badge = params.get('badge') or params.get('badge_name') or params.get('badgeName')
-		if username and badge:
-			# find user
-			user = User.get_user(username)
-			if user is None:
-				ent_catalog = component.getUtility(ICatalog, name=user_index.CATALOG_NAME)
-				results = list(ent_catalog.searchResults(email=(username, username)))
-				user = results[0] if results else None
-			if user is None:
-				raise hexc.HTTPNotFound('User not found')
-
-			# find assertion
-			manager = component.getUtility(badge_interfaces.IBadgeManager)
-			result = manager.get_assertion(user, badge)
-		else:
-			assertion_id = request.subpath[0] if request.subpath else ''
-			if not assertion_id:
-				raise hexc.HTTPNotFound()
-			assertion_id = urllib.unquote(assertion_id)
-			manager = component.getUtility(badge_interfaces.IBadgeManager)
-			result = manager.get_assertion_by_id(assertion_id)
-
-		if result is not None:
-			return open_interfaces.IBadgeAssertion(result)
-
-		raise hexc.HTTPNotFound('Assertion not found')
+		return self.request.context
 
 ALL_BADGES_VIEW = 'AllBadges'
 

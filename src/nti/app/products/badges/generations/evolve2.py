8a78d05b6187a2042fdf33ef62b22d62d7776d0c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-15 22:23:25 +0000

    evolve to generation 2 by adding an STL column to the badges db.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@43262 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/badges/generations/evolve2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+generation 2.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 2
+
+import sqlalchemy as sa
+
+from zope import component
+from zope.component.hooks import setHooks
+
+from alembic.operations import Operations
+from alembic.migration import MigrationContext
+
+from tahrir_api import model
+target_metadata = getattr(model.DeclarativeBase, 'metadata')
+
+from nti.badges.tahrir.interfaces import ITahrirBadgeManager
+
+def do_evolve(dscontext):
+	setHooks()
+	
+	manager = component.getUtility(ITahrirBadgeManager)
+	mc = MigrationContext.configure(manager.engine.connect())
+	op = Operations(mc)
+	
+	sa.Table('badges', mc, auto_load=True)
+	op.add_column("badges", sa.Column('stl', sa.Unicode(128)))
+
+def evolve(context):
+	"""
+	Evolve to generation 2 by adding the stl column
+	"""
+	do_evolve(context)

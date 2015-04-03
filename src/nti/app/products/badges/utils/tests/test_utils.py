70c7cb7bce4cd4e2e146490b29fb1823cf0e9414
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-03 02:45:17 +0000

    one more test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@62452 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/utils/tests/test_utils.py
+++ src/nti/app/products/badges/utils/tests/test_utils.py
@@ -16,7 +16,10 @@ import fudge
 
 from nti.app.products.badges.utils import get_badge_url
 from nti.app.products.badges.utils import get_badge_href
+from nti.app.products.badges.utils import get_openbadge_url
 from nti.app.products.badges.utils import get_badge_image_url
+
+from nti.app.products.badges.utils import get_assertion_url
 
 from nti.app.products.badges.tests import NTIBadgesTestCase
 
@@ -40,6 +43,9 @@ class TestUtils(NTIBadgesTestCase):
 		url = get_badge_url(badge, request)
 		assert_that(url, is_('http://localhost/dataserver2/OpenBadges/ichigo'))
 		
+		url = get_openbadge_url(badge, request)
+		assert_that(url, is_('http://localhost/dataserver2/OpenBadges/ichigo/badge.json'))
+		
 		url = get_badge_image_url(badge, request)
 		assert_that(url, is_('http://localhost/hosted_badge_images/ichigo.png'))
 		
@@ -50,3 +56,14 @@ class TestUtils(NTIBadgesTestCase):
 		badge.has_attr(image='tag_nextthought.com_2011-10_OU-HTML-CHEM4970_Chemistry_of_Beer.course_badge.png')
 		url = get_badge_image_url(badge, request)
 		assert_that(url, is_('http://localhost/hosted_badge_images/tag_nextthought.com_2011-10_OU-HTML-CHEM4970_Chemistry_of_Beer.course_badge.png'))
+		
+	def test_get_assertion_urls(self):
+		request = self._mock_request()
+		assertion = fudge.Fake()
+		assertion.has_attr(uid='xy##%6')
+		
+		href = get_assertion_url(assertion, request)
+		assert_that(href, is_('/dataserver2/OpenAssertions/xy%23%23%256'))
+		
+		href = get_assertion_url(assertion, request, True)
+		assert_that(href, is_('http://localhost/dataserver2/OpenAssertions/xy%23%23%256'))
